59222482b03a00755a7e63fa6899647476623539
andonirdgz <andoni_rdgz@outlook.com>
2022-09-26 16:22:57 -0700

    replaced not found env with process.env

--- config/plugins.js
+++ config/plugins.js
@@ -6,11 +6,11 @@ module.exports = () => {
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_ACCESS_SECRET,
+          region: process.env.AWS_REGION,
           params: {
-            Bucket: env('AWS_BUCKET_NAME')
+            Bucket: process.env.AWS_BUCKET_NAME
           }
         },
         // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
